1f8058a14dc2d838b1ad5815dc029c2ca7d8e278
martinaaa <mjokic00@fesb.hr>
2015-03-16 11:44:54 +0100

    lokalna promjena

--- pis1/Program.cs
+++ pis1/Program.cs
@@ -11,6 +11,7 @@ namespace pis1
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World_v2");
+            Console.WriteLine("lokalna promjena");
         }
     }
 }
